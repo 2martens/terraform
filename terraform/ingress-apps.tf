9178a00ba7cc7935b8648946ffbbb7ecdf0a69b8
Jim Martens <github@2martens.de>
2023-11-01 22:52:09 +0100

    Added domain for prometheus

--- terraform/ingress-apps.tf
+++ terraform/ingress-apps.tf
@@ -13,3 +13,11 @@ module "api_domain" {
   ipv4      = "49.13.77.70"
   ipv6      = "2a01:4f8:c012:4d25::1"
 }
+
+module "prometheus_k8s_test_domain" {
+  source    = "./modules/domain"
+  domain    = "2martens.de"
+  subdomain = "prometheus.k8s.test"
+  ipv4      = "49.13.77.70"
+  ipv6      = "2a01:4f8:c012:4d25::1"
+}
