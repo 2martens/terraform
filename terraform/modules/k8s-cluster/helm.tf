766de7e9a81d5901b8fcc6dbbd5b0b9bfd33afe8
Jim Martens <github@2martens.de>
2023-11-03 10:25:03 +0100

    Install argocd in high availability if at least three nodes are available

--- terraform/modules/k8s-cluster/helm.tf
+++ terraform/modules/k8s-cluster/helm.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "install_argocd" {
+resource "null_resource" "install_setup" {
   connection {
     host        = hcloud_server_network.manager_private[0].ip
     user        = "root"
@@ -10,8 +10,56 @@ resource "null_resource" "install_argocd" {
   provisioner "remote-exec" {
     inline = [
       "helm repo add 2martens https://repo.2martens.de/charts/",
-      "helm install 2martens/cluster_setup",
-      "helm install 2martens/argocd",
+      "helm install setup 2martens/cluster_setup",
+    ]
+  }
+
+  depends_on = [null_resource.join_nodes, hcloud_server.manager]
+}
+
+resource "null_resource" "install_argocd_single" {
+  count = var.number_nodes < 3 ? 1 : 0
+
+  connection {
+    host        = hcloud_server_network.manager_private[0].ip
+    user        = "root"
+    type        = "ssh"
+    private_key = var.terraform_private_ssh_key
+    timeout     = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "helm repo add 2martens https://repo.2martens.de/charts/",
+      "helm install setup 2martens/cluster_setup",
+      "helm install argocd 2martens/argocd",
+    ]
+  }
+
+  depends_on = [null_resource.join_nodes, hcloud_server.manager]
+}
+
+resource "null_resource" "install_argocd_ha" {
+  count = var.number_nodes >= 3 ? 1 : 0
+
+  connection {
+    host        = hcloud_server_network.manager_private[0].ip
+    user        = "root"
+    type        = "ssh"
+    private_key = var.terraform_private_ssh_key
+    timeout     = "2m"
+  }
+
+  provisioner "file" {
+    content     = file("${path.module}/templates/argocd-values-ha.yaml")
+    destination = "/home/terraform/argocd-values-ha.yaml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "helm repo add 2martens https://repo.2martens.de/charts/",
+      "helm install setup 2martens/cluster_setup",
+      "helm install argocd 2martens/argocd --values /home/terraform/argocd-values-ha.yaml",
     ]
   }
 
