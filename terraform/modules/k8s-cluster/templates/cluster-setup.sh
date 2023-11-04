3a424d359f399bfe9cb403c84f8cf1c654f883b1
Jim Martens <github@2martens.de>
2023-11-04 20:33:30 +0100

    Fix commands for helm

--- terraform/modules/k8s-cluster/templates/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/cluster-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-helm repo add 2martens https://repo.2martens.de/charts/
-helm install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
+microk8s helm repo add 2martens https://repo.2martens.de/charts/
+microk8s helm install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
 %{ if high_availability ~}
-helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/tmpfiles.d/argocd-values-ha.yaml
+microk8s helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/tmpfiles.d/argocd-values-ha.yaml
 %{ else ~}
-helm install argocd 2martens/argocd --set environment=${argocd_environment}
+microk8s helm install argocd 2martens/argocd --set environment=${argocd_environment}
 %{ endif ~}
