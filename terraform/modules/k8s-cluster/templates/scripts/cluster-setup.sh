e4ffd77ba2135a93457e0c69be024264a2596c14
Jim Martens <github@2martens.de>
2023-11-04 23:10:23 +0100

    Set kube config to 600

--- terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
@@ -6,6 +6,7 @@ done
 
 mkdir -p /home/${admin_user}/.kube
 /snap/bin/microk8s.kubectl config view > /home/${admin_user}/.kube/config
+chmod 600 /home/${admin_user}/.kube/config
 chown -R ${admin_user}:${admin_user} /home/2martensAdmin/.kube
 /snap/bin/microk8s.helm3 repo add 2martens https://repo.2martens.de/charts/
 /snap/bin/microk8s.helm3 install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
