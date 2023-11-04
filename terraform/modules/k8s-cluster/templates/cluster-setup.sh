9b616faae59bb5439524ca2ea1722bd92fc6dcdf
Jim Martens <github@2martens.de>
2023-11-04 20:26:01 +0100

    Fixed path for cluster setup

--- terraform/modules/k8s-cluster/templates/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/cluster-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 helm repo add 2martens https://repo.2martens.de/charts/
-helm install setup 2martens/cluster_setup --values /run/terraform/cluster-setup-values.yaml
+helm install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
 %{ if high_availability ~}
 helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/tmpfiles.d/argocd-values-ha.yaml
 %{ else ~}
