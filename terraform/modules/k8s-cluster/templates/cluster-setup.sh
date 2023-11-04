25c9aefea83850ef74826c9b30fc7731f0bba2ea
Jim Martens <github@2martens.de>
2023-11-04 19:32:06 +0100

    Try saving files in existing tmpfile directory

--- terraform/modules/k8s-cluster/templates/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/cluster-setup.sh
@@ -3,7 +3,7 @@
 helm repo add 2martens https://repo.2martens.de/charts/
 helm install setup 2martens/cluster_setup --values /run/terraform/cluster-setup-values.yaml
 %{ if high_availability ~}
-helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/terraform/argocd-values-ha.yaml
+helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/tmpfiles.d/argocd-values-ha.yaml
 %{ else ~}
 helm install argocd 2martens/argocd --set environment=${argocd_environment}
 %{ endif ~}
