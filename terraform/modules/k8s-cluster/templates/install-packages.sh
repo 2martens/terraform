dadc8925ae46395fffd25f0098971b8b05240cb9
Jim Martens <github@2martens.de>
2023-11-04 20:21:47 +0100

    Use apt-get

--- terraform/modules/k8s-cluster/templates/install-packages.sh
+++ terraform/modules/k8s-cluster/templates/install-packages.sh
@@ -5,5 +5,5 @@ apt-get -y install ufw
 apt-get -y install snapd
 
 curl https://packages.hetzner.com/hcloud/deb/hc-utils_0.0.4-1_all.deb -o /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb -s
-apt install /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb
+apt-get -y install /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb
 snap install kubectl --classic
