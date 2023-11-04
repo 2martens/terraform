499bdea97d527d536efb9d082f5741c94a762dc4
Jim Martens <github@2martens.de>
2023-11-04 23:46:05 +0100

    Use admin user correctly

--- terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
@@ -7,7 +7,7 @@ done
 mkdir -p /home/${admin_user}/.kube
 cp /var/snap/current/credentials/client.config /home/${admin_user}/.kube/config
 chmod 600 /home/${admin_user}/.kube/config
-chown -R ${admin_user}:${admin_user} /home/2martensAdmin/.kube
+chown -R ${admin_user}:${admin_user} /home/${admin_user}/.kube
 /snap/bin/microk8s.helm3 repo add 2martens https://repo.2martens.de/charts/
 /snap/bin/microk8s.helm3 install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
 %{ if high_availability ~}
