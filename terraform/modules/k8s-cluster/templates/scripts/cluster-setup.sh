b4c6d4822bc6bc6ef7f7d9c352e1f87eb2a12d69
Jim Martens <github@2martens.de>
2023-11-04 22:21:04 +0100

    Wait for microk8s before attempting to install further things

--- terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+until /snap/bin/microk8s.status --wait-ready;
+  do sleep 3; echo "waiting for worker status..";
+done
+
 /snap/bin/microk8s helm repo add 2martens https://repo.2martens.de/charts/
 /snap/bin/microk8s helm install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
 %{ if high_availability ~}
