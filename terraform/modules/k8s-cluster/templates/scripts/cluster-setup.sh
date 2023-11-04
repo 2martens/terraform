60897fa08a13478f854d8e717e886ab93681dc40
Jim Martens <github@2martens.de>
2023-11-04 22:30:49 +0100

    Use helm 3

--- terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/scripts/cluster-setup.sh
@@ -4,10 +4,10 @@ until /snap/bin/microk8s.status --wait-ready;
   do sleep 3; echo "waiting for worker status..";
 done
 
-/snap/bin/microk8s helm repo add 2martens https://repo.2martens.de/charts/
-/snap/bin/microk8s helm install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
+/snap/bin/microk8s.helm3 repo add 2martens https://repo.2martens.de/charts/
+/snap/bin/microk8s.helm3 install setup 2martens/cluster_setup --values /run/tmpfiles.d/cluster-setup-values.yaml
 %{ if high_availability ~}
-/snap/bin/microk8s helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/tmpfiles.d/argocd-values-ha.yaml
+/snap/bin/microk8s.helm3 install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/tmpfiles.d/argocd-values-ha.yaml
 %{ else ~}
-/snap/bin/microk8s helm install argocd 2martens/argocd --set environment=${argocd_environment}
+/snap/bin/microk8s.helm3 install argocd 2martens/argocd --set environment=${argocd_environment}
 %{ endif ~}
