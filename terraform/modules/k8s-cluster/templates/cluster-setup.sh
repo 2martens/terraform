6c89fc664624a6e1bbd2deae54cda21d518f2a46
Jim Martens <github@2martens.de>
2023-11-04 19:14:27 +0100

    Fixed cloud init scripts

--- terraform/modules/k8s-cluster/templates/cluster-setup.sh
+++ terraform/modules/k8s-cluster/templates/cluster-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 helm repo add 2martens https://repo.2martens.de/charts/
-helm install setup 2martens/cluster_setup --values /home/terraform/cluster-setup-values.yaml
+helm install setup 2martens/cluster_setup --values /run/terraform/cluster-setup-values.yaml
 %{ if high_availability ~}
-helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /home/terraform/argocd-values-ha.yaml
+helm install argocd 2martens/argocd --set environment=${argocd_environment} --values /run/terraform/argocd-values-ha.yaml
 %{ else ~}
 helm install argocd 2martens/argocd --set environment=${argocd_environment}
 %{ endif ~}
--- terraform/modules/k8s-cluster/templates/microk8s-setup.sh
+++ terraform/modules/k8s-cluster/templates/microk8s-setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 mkdir -p /var/snap/microk8s/common/etc/launcher
-cp /home/terraform/.microk8s.yaml /var/snap/microk8s/common/etc/launcher/.microk8s.yaml
-chown root:root /var/snap/microk8s/common/etc/launcher/.microk8s.yaml
+cp /run/terraform/.microk8s.yaml /var/snap/microk8s/common/etc/launcher/.microk8s.yaml
 %{ if !main_node ~}
 sleep 120
 %{ endif ~}
