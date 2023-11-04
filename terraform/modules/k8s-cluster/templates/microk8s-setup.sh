c6b6023094ed4a027a38bd798e4c332ea02af6cd
Jim Martens <github@2martens.de>
2023-11-04 20:44:37 +0100

    Try launch configuration as non-hidden file

--- terraform/modules/k8s-cluster/templates/microk8s-setup.sh
+++ terraform/modules/k8s-cluster/templates/microk8s-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /var/snap/microk8s/common/etc/launcher
-cp /run/tmpfiles.d/.microk8s.yaml /var/snap/microk8s/common/etc/launcher/.microk8s.yaml
+cp /run/tmpfiles.d/microk8s.yaml /var/snap/microk8s/common/etc/launcher/microk8s.yaml
 %{ if !main_node ~}
 sleep 120
 %{ endif ~}
