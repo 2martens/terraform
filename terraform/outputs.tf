7cd1da298ab5580828254c73645b9d32ded1a507
Jim Martens <github@2martens.de>
2023-11-05 10:55:46 +0100

    Modified outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "ansible_inventory" {
     devops = hcloud_server.server_devops.ipv4_address,
     k8s_servers = concat(
       tolist([hcloud_server.server_k8s_test.ipv4_address]),
-      #      module.test_cluster.node_ipv6_addresses
+      module.test_cluster.node_ipv6_addresses
     )
   })
 }
