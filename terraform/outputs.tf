83413b72e9d99d87120a93eff3a9764a7b9f77fa
Jim Martens <github@2martens.de>
2023-11-05 12:10:52 +0100

    Remove server from output but reinstate itself

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,6 @@ output "ansible_inventory" {
   value = templatefile("${path.module}/templates/inventory.tftpl", {
     devops = hcloud_server.server_devops.ipv4_address,
     k8s_servers = concat(
-      tolist([hcloud_server.server_k8s_test.ipv4_address]),
       module.test_cluster.node_ipv6_addresses
     )
   })
