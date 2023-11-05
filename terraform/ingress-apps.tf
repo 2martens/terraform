bac3e0c171b820cb77b60369ce4b54c077c3482b
Jim Martens <github@2martens.de>
2023-11-05 10:54:10 +0100

    Changed IPs for domains

--- terraform/ingress-apps.tf
+++ terraform/ingress-apps.tf
@@ -10,14 +10,14 @@ module "api_domain" {
   source    = "./modules/domain"
   domain    = local.domain
   subdomain = "api"
-  ipv4      = "49.13.77.70"
-  ipv6      = "2a01:4f8:c012:4d25::1"
+  ipv4      = module.test_cluster.api_server_ipv4_address
+  ipv6      = module.test_cluster.api_server_ipv6_address
 }
 
 module "prometheus_k8s_test_domain" {
   source    = "./modules/domain"
   domain    = local.domain
   subdomain = "prometheus.k8s.test"
-  ipv4      = "49.13.77.70"
-  ipv6      = "2a01:4f8:c012:4d25::1"
+  ipv4      = module.test_cluster.api_server_ipv4_address
+  ipv6      = module.test_cluster.api_server_ipv6_address
 }
