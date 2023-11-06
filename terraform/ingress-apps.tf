0120b80c6ce60aece34f2b70c7ec35cced493c8b
Jim Martens <github@2martens.de>
2023-11-06 22:50:19 +0100

    Add new domain

--- terraform/ingress-apps.tf
+++ terraform/ingress-apps.tf
@@ -13,3 +13,11 @@ module "prometheus_k8s_test_domain" {
   ipv4      = module.test_cluster.api_server_ipv4_address
   ipv6      = module.test_cluster.api_server_ipv6_address
 }
+
+module "prometheus_k8s_test_domain" {
+  source    = "./modules/domain"
+  domain    = local.domain
+  subdomain = "thanos.prometheus.k8s.test"
+  ipv4      = module.test_cluster.api_server_ipv4_address
+  ipv6      = module.test_cluster.api_server_ipv6_address
+}
