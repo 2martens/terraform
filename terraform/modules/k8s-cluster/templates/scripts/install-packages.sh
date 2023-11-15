1a2bb5adb3698bf2f29d14f8390afc9621ad1f3b
Jim Martens <github@2martens.de>
2023-11-15 22:38:39 +0100

    Install newer kernel

--- terraform/modules/k8s-cluster/templates/scripts/install-packages.sh
+++ terraform/modules/k8s-cluster/templates/scripts/install-packages.sh
@@ -3,6 +3,7 @@
 apt-get -y install fail2ban
 apt-get -y install ufw
 apt-get -y install snapd
+apt-get -y install linux-generic-hwe-22.04
 
 curl https://packages.hetzner.com/hcloud/deb/hc-utils_0.0.4-1_all.deb -o /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb -s
 apt-get -y install /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb
