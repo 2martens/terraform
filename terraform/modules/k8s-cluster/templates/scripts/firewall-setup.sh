63522a30fbbb1c1eb0be2c737661e91a35ed93a7
Jim Martens <github@2martens.de>
2023-11-15 14:03:39 +0100

    Allow cilium ports

--- terraform/modules/k8s-cluster/templates/scripts/firewall-setup.sh
+++ terraform/modules/k8s-cluster/templates/scripts/firewall-setup.sh
@@ -7,7 +7,10 @@ ufw allow OpenSSH
 ufw allow 16443 comment "Kubernetes API endpoint"
 ufw allow 80 comment HTTP
 ufw allow 443 comment HTTPS
+ufw allow to ${node_ip} port 4244 comment "Hubble peer endpoint"
 ufw allow to ${node_ip} port 9100 comment "Metrics endpoint"
+ufw allow to ${node_ip} port 9964 comment "Cilium agent endpoint"
+ufw allow to ${node_ip} port 9965 comment "Hubble metrics endpoint"
 ufw allow to ${node_ip} port 10249 comment "kube-proxy metrics port"
 ufw allow to ${node_ip} port 10250 comment "cadvisor port"
 ufw allow to ${node_ip} port 10259 comment "kube-scheduler port"
