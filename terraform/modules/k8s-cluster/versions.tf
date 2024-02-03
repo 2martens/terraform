6be13eb12b3270a21dbf2f50fb465046a7da62fd
Jim Martens <github@2martens.de>
2024-02-03 19:33:38 +0100

    chore: Update inwx provider

--- terraform/modules/k8s-cluster/versions.tf
+++ terraform/modules/k8s-cluster/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     inwx = {
       source  = "inwx/inwx"
-      version = "1.3.2"
+      version = "1.4.0"
     }
     hcloud = {
       source  = "hetznercloud/hcloud"
