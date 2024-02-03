4ff702e1e988b5f058b13d7300c98d21795dc316
Jim Martens <github@2martens.de>
2024-02-03 22:34:38 +0100

    chore: Add grafana subdomain

--- terraform/ingress-apps.tf
+++ terraform/ingress-apps.tf
@@ -21,3 +21,11 @@ module "thanos_prometheus_k8s_test_domain" {
   ipv4      = module.test_cluster.api_server_ipv4_address
   ipv6      = module.test_cluster.api_server_ipv6_address
 }
+
+module "grafana_k8s_monitoring_domain" {
+  source    = "./modules/domain"
+  domain    = local.domain
+  subdomain = "grafana.k8s.monitoring"
+  ipv4      = module.monitoring_cluster.api_server_ipv4_address
+  ipv6      = module.monitoring_cluster.api_server_ipv6_address
+}
