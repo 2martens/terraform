1b7969bada6492b1321cb4483fe210e02373720f
Jim Martens <github@2martens.de>
2024-09-04 08:46:46 +0200

    fix: Remove outputs from domain-test

--- domain-test/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "ansible_inventory" {
-  value = templatefile("${path.module}/templates/inventory.tftpl", {
-    devops = hcloud_server.server_devops.ipv4_address,
-    k8s_servers = concat(
-      module.test_cluster.node_ipv6_addresses
-    )
-  })
-}
