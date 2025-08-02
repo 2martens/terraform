fa5832dcdd01ad4b0ef1d48e748cf2e3ba1866cd
Jim Martens <github@2martens.de>
2025-08-02 22:00:49 +0200

    fix: Add certbot package

--- terraform/modules/docker-network/templates/scripts/install-packages.sh
+++ terraform/modules/docker-network/templates/scripts/install-packages.sh
@@ -21,3 +21,5 @@ apt-get -y install ufw
 
 curl https://packages.hetzner.com/hcloud/deb/hc-utils_0.0.4-1_all.deb -o /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb -s
 apt-get -y install /run/tmpfiles.d/hc-utils_0.0.4-1_all.deb
+
+apt-get -y install certbot
