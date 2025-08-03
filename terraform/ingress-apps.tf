0a851ca2aad9534c16ac38587404ebabb6810e64
Jim Martens <github@2martens.de>
2025-08-03 15:19:44 +0200

    fix: Remove all K8s related resources

--- terraform/ingress-apps.tf
+++ terraform/ingress-apps.tf
@@ -1,31 +1,31 @@
-module "api_domain" {
-  source    = "./modules/domain"
-  domain    = local.domain
-  subdomain = "api"
-  ipv4      = module.test_cluster.api_server_ipv4_address
-  ipv6      = module.test_cluster.api_server_ipv6_address
-}
-
-module "prometheus_k8s_test_domain" {
-  source    = "./modules/domain"
-  domain    = local.domain
-  subdomain = "prometheus.k8s.test"
-  ipv4      = module.test_cluster.api_server_ipv4_address
-  ipv6      = module.test_cluster.api_server_ipv6_address
-}
-
-module "thanos_prometheus_k8s_test_domain" {
-  source    = "./modules/domain"
-  domain    = local.domain
-  subdomain = "thanos.prometheus.k8s.test"
-  ipv4      = module.test_cluster.api_server_ipv4_address
-  ipv6      = module.test_cluster.api_server_ipv6_address
-}
-
-module "grafana_k8s_monitoring_domain" {
-  source    = "./modules/domain"
-  domain    = local.domain
-  subdomain = "grafana.k8s.monitoring"
-  ipv4      = module.monitoring_cluster.api_server_ipv4_address
-  ipv6      = module.monitoring_cluster.api_server_ipv6_address
-}
+# module "api_domain" {
+#   source    = "./modules/domain"
+#   domain    = local.domain
+#   subdomain = "api"
+#   ipv4      = module.test_cluster.api_server_ipv4_address
+#   ipv6      = module.test_cluster.api_server_ipv6_address
+# }
+#
+# module "prometheus_k8s_test_domain" {
+#   source    = "./modules/domain"
+#   domain    = local.domain
+#   subdomain = "prometheus.k8s.test"
+#   ipv4      = module.test_cluster.api_server_ipv4_address
+#   ipv6      = module.test_cluster.api_server_ipv6_address
+# }
+#
+# module "thanos_prometheus_k8s_test_domain" {
+#   source    = "./modules/domain"
+#   domain    = local.domain
+#   subdomain = "thanos.prometheus.k8s.test"
+#   ipv4      = module.test_cluster.api_server_ipv4_address
+#   ipv6      = module.test_cluster.api_server_ipv6_address
+# }
+#
+# module "grafana_k8s_monitoring_domain" {
+#   source    = "./modules/domain"
+#   domain    = local.domain
+#   subdomain = "grafana.k8s.monitoring"
+#   ipv4      = module.monitoring_cluster.api_server_ipv4_address
+#   ipv6      = module.monitoring_cluster.api_server_ipv6_address
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "ansible_inventory" {
-  value = templatefile("${path.module}/templates/inventory.tftpl", {
-    devops = hcloud_server.server_devops.ipv4_address,
-    k8s_servers = concat(
-      module.test_cluster.node_ipv6_addresses
-    )
-  })
-}
+# output "ansible_inventory" {
+#   value = templatefile("${path.module}/templates/inventory.tftpl", {
+#     devops = hcloud_server.server_devops.ipv4_address,
+#     k8s_servers = concat(
+#       module.test_cluster.node_ipv6_addresses
+#     )
+#   })
+# }
 
 output "swarm_nodes" {
   value = module.test_swarm_cluster.node_details
