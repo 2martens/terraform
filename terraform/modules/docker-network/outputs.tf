d23e3c15755956071021b5e62c1dbd7f65540fdd
Jim Martens <github@2martens.de>
2025-08-03 16:08:30 +0200

    fix: Consider count for output

--- terraform/modules/docker-network/outputs.tf
+++ terraform/modules/docker-network/outputs.tf
@@ -2,10 +2,10 @@ output "node_details" {
   value = {
     for idx, instance in hcloud_server.node :
     "node-${idx}" => {
-      id      = instance.id
-      name    = instance.name
-      ipv4    = instance.ipv4_address
-      ipv6    = instance.ipv6_address
+      id   = instance.id
+      name = instance.name
+      ipv4 = instance.ipv4_address
+      ipv6 = instance.ipv6_address
     }
   }
 }
@@ -14,17 +14,20 @@ output "worker_node_details" {
   value = {
     for idx, instance in hcloud_server.worker_node :
     "worker-node-${idx}" => {
-      id      = instance.id
-      name    = instance.name
-      ipv4    = instance.ipv4_address
-      ipv6    = instance.ipv6_address
+      id   = instance.id
+      name = instance.name
+      ipv4 = instance.ipv4_address
+      ipv6 = instance.ipv6_address
     }
   }
 }
 
 output "loadbalancer_details" {
   value = {
-    ipv4 = hcloud_load_balancer.docker.ipv4
-    ipv6 = hcloud_load_balancer.docker.ipv6
+    for idx, instance in hcloud_load_balancer.docker :
+    "loadbalancer-${idx}" => {
+      ipv4 = instance.ipv4
+      ipv6 = instance.ipv6
+    }
   }
 }
